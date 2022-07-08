fcc5ee5435b8e924ec38c556e130e8e87c4f127f
Arman <95692425+ArmanKhoyetsyan@users.noreply.github.com>
2022-07-08 21:14:36 +0400

    asd

--- sessionExpress.js
+++ sessionExpress.js
@@ -7,7 +7,7 @@ const sessionExpress = () => expressSession({
   cookie: {
     originalMaxAge: null,
     httpOnly: false,
-    sameSite:'None',
+    sameSite:'none',
     secure:undefined,
   },
 });
