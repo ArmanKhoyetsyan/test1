2d1a3b0b118deb0f6882ecd006d142ee53957df9
dwintechnology <49997178+dwintechnology@users.noreply.github.com>
2022-07-08 19:12:52 +0400

    asdasdasd

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -9,6 +9,10 @@ router.post('/login', express.urlencoded({ extended: false }), (req, res) => {
     ) {
       req.session.user = req.body;
       req.session.save(() => {
+            res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
         res.status(200).json({ msg: 'Welcome' });
       });
     } else {
