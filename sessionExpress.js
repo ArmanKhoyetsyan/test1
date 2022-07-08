8198a973ace4ba0dd0561e7e4fa37b281e610104
ArmanKhoyetsyan <arman.khoyetsyan.dwin@gmail.com>
2022-07-08 15:57:33 +0400

    s

--- sessionExpress.js
+++ sessionExpress.js
@@ -1,7 +1,7 @@
 const expressSession = require('express-session');
 
 const sessionExpress = () => expressSession({
-  secret:'sasaasdasd',
+  secret:process.env.SESSION_SECRET,
   saveUninitialized: true,
   resave: true,
   cookie: {
