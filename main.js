bf18d064f6085a6418c44283044abe3672f212ac
Arman <95692425+ArmanKhoyetsyan@users.noreply.github.com>
2022-07-11 12:17:10 +0400

    Update main.js

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ const port = process.env.PORT || 3000;
 const whitelist = ['http://virtual-exchange-admin.herokuapp.com', 'https://virtual-exchange-admin.herokuapp.com'];
 
 const corsOptions = {
-  credentials:'include',
+  credentials:true,
   origin: whitelist,
   exposedHeaders: ["set-cookie"],
 };
